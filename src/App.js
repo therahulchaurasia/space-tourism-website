15b32fabae6475d1d134791ad9daac356a7571bd
Rahul <therahulchaurasia@gmail.com>
2023-09-09 22:39:26 +0530

    Created pages and added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import data from './data.json'
+import Home from './pages/Home'
+import Destination from './pages/Destination'
+import Crew from './pages/Crew'
+import Technology from './pages/Technology'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+//TODO: Create a custom not found page for this project.
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/destination',
+    element: <Destination />,
+  },
+  {
+    path: '/crew',
+    element: <Crew />,
+  },
+  {
+    path: '/technology',
+    element: <Technology />,
+  },
+])
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +42,11 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <RouterProvider router={router} />
+      {/* <h1 className="text-2xl font-bold underline">Hello world!</h1> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      barlow: ['Barlow Condensed', 'sans-serif'],
+      bellefair: ['Bellefair', 'serif'],
+    },
+    extend: {
+      colors: {
+        lilac: '#D0D6F9',
+        jetBlack: '#0B0D17',
+        white: '#FFFFFF',
+      },
+    },
+  },
+  plugins: [],
+}
