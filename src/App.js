614d2110847b08d552dd518dde3373e77de6c908
Rahul <therahulchaurasia@gmail.com>
2023-09-10 23:14:11 +0530

    Addition by removal

--- src/App.js
+++ src/App.js
@@ -29,22 +29,7 @@ const router = createBrowserRouter([
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <RouterProvider router={router} />
-      {/* <h1 className="text-2xl font-bold underline">Hello world!</h1> */}
     </div>
   )
 }
