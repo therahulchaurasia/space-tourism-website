879da2b8fd2c2c7053793526cbd8e632fbbfda70
Rahul <therahulchaurasia@gmail.com>
2023-09-12 23:05:23 +0530

    Image done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import bgMobile from '../assets/home/background-home-mobile.jpg'
 const Home = () => {
   return (
     <div className="">
-      <div className="bg-heroMobile"></div>
-      {/* <h2 className="">Home</h2> */}
+      <div className="bg-heroMobile md:bg-heroTablet xl:bg-heroDesktop bg-no-repeat bg-cover w-screen h-screen"></div>
+      <header></header>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 import bgMobile from './src/assets/home/background-home-mobile.jpg'
+import bgTablet from './src/assets/home/background-home-tablet.jpg'
+import bgDesktop from './src/assets/home/background-home-desktop.jpg'
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
@@ -15,6 +17,8 @@ module.exports = {
       },
       backgroundImage: {
         heroMobile: "url('/src/assets/home/background-home-mobile.jpg')",
+        heroTablet: "url('/src/assets/home/background-home-tablet.jpg')",
+        heroDesktop: "url('/src/assets/home/background-home-desktop.jpg')",
       },
     },
   },
