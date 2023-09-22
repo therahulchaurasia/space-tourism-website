ae89308c25ee19948df6ed033b369012d474d053
Rahul <therahulchaurasia@gmail.com>
2023-09-22 23:05:25 +0530

    Added toggle functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/shared/logo.svg'
 import hamburger from '../assets/shared/icon-hamburger.svg'
 import close from '../assets/shared/icon-close.svg'
+import classNames from 'classnames'
 const Navbar = () => {
+  const [toggleNavBar, setToggleNavBar] = useState(false)
   return (
     <div>
-      <navbar className="flex justify-between items-center mx-6 my-6">
+      <nav className="flex justify-between items-center mx-6 my-6">
         <img src={logo} alt="Logo" className="h-auto max-w-full" />
         <img
           src={hamburger}
           alt="hamburger"
           className="h-auto max-w-full"
-          // onClick={() => {
-          //   console.log('HEllo')
-          // }}
+          onClick={() => {
+            setToggleNavBar(!toggleNavBar)
+          }}
         />
-      </navbar>
-      <aside className="bg-blue-500 w-2/3 h-screen fixed top-0 right-0 bg-opacity-10 backdrop-blur-2xl">
+      </nav>
+      <aside
+        className={classNames(
+          'bg-blue-500 w-2/3 h-screen fixed top-0 right-0 bg-opacity-10 backdrop-blur-2xl',
+          { hidden: toggleNavBar, fixed: !toggleNavBar }
+        )}
+      >
         <div className="my-6 mx-6 flex items-center justify-end">
-          <img src={close} alt="Close" className="h-auto max-w-full" />
+          <img
+            src={close}
+            alt="Close"
+            className="h-auto max-w-full"
+            onClick={() => {
+              setToggleNavBar(!toggleNavBar)
+            }}
+          />
         </div>
         <div className="uppercase text-white mt-8 flex flex-col gap-y-4 font-barlow px-10">
           <h4>
