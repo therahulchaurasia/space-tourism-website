9fdd5321c144e02f00a0871eeb2ba90875370854
therahulchaurasia <therahulchaurasia@gmail.com>
2023-09-26 18:48:32 +0530

    Technology done
    -Moon is overlapping. Needs to be fixed

--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -4,41 +4,41 @@ import rocketld from '../assets/technology/image-launch-vehicle-landscape.jpg'
 import rocketpt from '../assets/technology/image-launch-vehicle-portrait.jpg'
 const Technology = () => {
   return (
-    <div className="bg-techMobile md:bg-techTablet xl:bg-techMobile bg-no-repeat bg-cover w-full max-w-screen min-h-screen absolute">
+    <div className="bg-techMobile md:bg-techTablet xl:bg-techDesktop bg-no-repeat bg-cover w-full max-w-screen min-h-screen absolute">
       <Navbar />
-      <h6 className="text-white font-barlow uppercase tracking-widest mb-8  text-center md:text-xl  xl:mt-8 xl:text-start">
+      <h6 className="text-white font-barlow uppercase tracking-widest mb-8  text-center md:text-xl  xl:mt-8 xl:text-start xl:mb-0 xl:mx-auto xl:w-11/12 xl:pl-10">
         <span className="text-gray-500 font-bold pr-2">03</span> Space launch
         101
       </h6>
-      <div className="flex flex-col xl:flex-row xl:justify-evenly">
+      <div className="flex flex-col xl:flex-row xl:justify-between xl:items-center">
         <div className="mb-8">
           <img src={rocketld} alt="" className="w-full h-auto xl:hidden" />
         </div>
-        <div className="mb-8 xl:order-3 xl:w-3/12">
+        <div className="mb-8 xl:order-3 xl:w-4/12">
           <img
             src={rocketpt}
             alt=""
             className="hidden w-full h-auto xl:block"
           />
         </div>
-        <div className="flex justify-between mx-auto w-2/5 mb-6 sm:w-3/12 xl:flex-col xl:w-1/12">
-          <button className="bg-white w-10 h-10 rounded-full xl:w-14 xl:h-14">
+        <div className="flex justify-between mx-auto w-2/5 mb-6 sm:w-3/12 xl:flex-col xl:w-1/12 xl:m-0 xl:items-end gap-y-8 font-bellefair xl:text-2xl">
+          <button className="bg-white w-10 h-10 rounded-full xl:w-14 xl:h-14 ">
             1
           </button>
           <button className="text-white border border-gray-600 w-10 h-10 rounded-full xl:w-14 xl:h-14">
             {/* TODO: border-l. looks cool */}2
           </button>
-          <button className="text-white  w-10 h-10 rounded-full xl:w-14 xl:h-14">
+          <button className="text-white  w-10 h-10 rounded-full border-l xl:w-14 xl:h-14">
             3
           </button>
         </div>
-        <div className=" text-center mb-8 xl:text-start xl:w-5/12">
+        <div className=" text-center mb-8 xl:text-start xl:w-6/12">
           <h6 className="uppercase font-barlow text-lilac tracking-widest mb-2">
             The Terminology ...
           </h6>
           <h4
             className="uppercase font-bellefair text-white 
-        text-2xl mb-4 md:text-4xl"
+        text-2xl mb-4 md:text-4xl xl:text-5xl"
           >
             Launch Vehicle
           </h4>
